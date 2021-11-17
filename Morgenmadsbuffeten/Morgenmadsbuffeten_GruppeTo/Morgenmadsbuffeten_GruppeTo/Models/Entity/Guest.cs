7bb8ec8ac0d953c02a2a5443baa1e16ef679e6f7
AndreasErslev <andreas.erslev@gmail.com>
2021-11-17 12:00:50 +0100

    Oprettelse af projekt + database

---
+++ Morgenmadsbuffeten/Morgenmadsbuffeten_GruppeTo/Morgenmadsbuffeten_GruppeTo/Models/Entity/Guest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Morgenmadsbuffeten_GruppeTo.Models.Entity
+{
+    public class Guest 
+    {
+        [Key]
+        public int GuestId { get; set; }
+        public string RoomNbr { get; set; }
+        public int AdultCount { get; set; }
+        public int ChildCount { get; set; }
+    }
+}
