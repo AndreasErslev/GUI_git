e6e9ec6082464666eef85455d718df281a8bc4c5
Johanne <johanneberg2@gmail.com>
2021-11-19 17:18:05 +0100

    GUI aflevering som virker

---
+++ BreakfastBuffet2/BreakfastBuffet2/Models/Entity/Guest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Morgenmadsbuffeten_GruppeTo.Models.Entity
+{
+    public class Guest 
+    {
+        [Key]
+        public int GuestId { get; set; }
+        public string Room_Number { get; set; }
+        public bool Adults { get; set; }
+        public bool Children { get; set; }
+        public bool Book { get; set; }
+        public bool Check_In { get; set; }
+
+    }
+}
---
+++ BreakfastBuffet2/BreakfastBuffet2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BreakfastBuffet2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
