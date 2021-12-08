e2313c98a92dff95d962bf4c3faeab54b6428acd
Johanne <johanneberg2@gmail.com>
2021-12-08 10:58:05 +0100

    jwt

--- Assigment3/modelsmanagement_grp2/src/views/Login.vue
+++ Assigment3/modelsmanagement_grp2/src/views/Login.vue
@@ -75,7 +75,42 @@ export default {
       this.password = "";
     },
   },
-};
+    async login() {
+        let url = "https://localhost:44368/api/account/login";
+        try {
+            let response = await fetch(url, {
+                method: "POST",
+                body: JSON.stringify(this.form), // Assumes data is in an object called form
+                headers: new Headers({
+                    "Content-Type": "application/json"
+                })
+            });
+            if (response.ok) {
+                let token = await response.json();
+                localStorage.setItem("token", token.jwt);
+                // Change view to some other component
+                let payload = parseJwt(token);
+                localStorage.setItem("email", payload.schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress);
+                //Set id og rolle
+                
+            } else {
+                alert("Server returned: " + response.statusText);
+            }
+        } catch (err) {
+            alert("Error: " + err);
+        }
+        return;
+        },
+        function parseJwt(token) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        return JSON.parse(jsonPayload);
+    };
+    };
 </script>
 
 
